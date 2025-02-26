977068a2f850ea10f10fae2c5b627aa850af0c8b
Potato228 <petruhakabachok@gmail.com>
2025-02-26 23:36:40 +0800

    auction pages added

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index'
-import store from './store/index'
-//Подключаю стили
-import '@/assets/css/base.css'
-import '@/assets/css/bootstrap.min.css'
-import '@/assets/css/feather-icon.css'
-import '@/assets/css/sticky-footer.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index";
+import store from "./store/index";
 
-createApp(App).use(router).use(store).mount('#app')
+// Styles
+import "@/assets/css/zero.css";
+import "@/assets/css/base.css";
+import "@/assets/css/feather-icon.css";
+import "@/assets/css/sticky-footer.css";
+
+createApp(App).use(router).use(store).mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import * as VueRouter from "vue-router";
 
+import store from "@/store";
+
 import MainPage from "./pages/MainPage.vue";
 import AboutPage from "./pages/AboutPage.vue";
 import ContactsPage from "./pages/ContactsPage.vue";
@@ -10,8 +12,8 @@ import MyLayout from "./pages/MyLayout.vue";
 import AdminCabinet from "./pages/admin/AdminCabinet.vue";
 import AdminMarks from "./pages/admin/AdminMarks.vue";
 import EditProfile from "./pages/admin/EditProfile.vue";
-
-import store from "@/store";
+import AuctionPage from "./pages/auction/AuctionPage.vue";
+import AuctionPromo from "./pages/auction/AuctionPromo.vue";
 
 export default VueRouter.createRouter({
 	history: VueRouter.createWebHistory(),
@@ -45,6 +47,23 @@ export default VueRouter.createRouter({
 					path: "/projectsList",
 					name: "projectsListPage",
 					component: ProjectsListPage,
+				},
+				{
+					path: "/auction",
+					name: "auctionPage",
+					component: AuctionPage,
+					beforeEnter: (to, from, next) => {
+						if (!store.getters.getLogged) {
+							next({ path: "/auction/promo" });
+						} else {
+							next();
+						}
+					},
+				},
+				{
+					path: "/auction/promo",
+					name: "auctionPromo",
+					component: AuctionPromo,
 				},
 			],
 		},
