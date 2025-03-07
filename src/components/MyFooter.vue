bd6213679ea15deb9f264f36370b274779d2d635
Potato228 <petruhakabachok@gmail.com>
2025-03-07 08:22:17 +0800

    footer file formatted

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,25 +1,33 @@
 <template>
-    <footer class="footer">
-				<ul class="nav d-flex justify-content-center border-bottom gap-3">
-					<li v-for="(item,index) in menuItems" :key="index" class="nav-item">
-                        <router-link :to="{path:item.link}" class="nav-link px-2 text-body-secondary">{{ item.title }}</router-link>
-                    </li>					
-				</ul>
-				<p class="text-center text-body-secondary"><slot></slot></p>
-			</footer>
+	<footer class="footer">
+		<ul class="nav d-flex justify-content-center border-bottom gap-3">
+			<li
+				v-for="(item, index) in menuItems"
+				:key="index"
+				class="nav-item"
+			>
+				<router-link
+					:to="{ path: item.link }"
+					class="nav-link px-2 text-body-secondary"
+					>{{ item.title }}</router-link
+				>
+			</li>
+		</ul>
+		<p class="text-center text-body-secondary"><slot></slot></p>
+	</footer>
 </template>
 <script>
-export default{
-    name:'MyFooter',
-	data(){
-        return{
-            menuItems:[
-                {'link':'/','title':'Главная'},
-                {'link':'/about','title':'О нас'},
-                {'link':'/projectsList','title':'Список проектов'},
-                {'link':'/contacts','title':'Контакты'},
-            ],
-        }
-    }
-}
+	export default {
+		name: "MyFooter",
+		data() {
+			return {
+				menuItems: [
+					{ link: "/", title: "Главная" },
+					{ link: "/about", title: "О нас" },
+					{ link: "/projectsList", title: "Список проектов" },
+					{ link: "/contacts", title: "Контакты" },
+				],
+			};
+		},
+	};
 </script>
